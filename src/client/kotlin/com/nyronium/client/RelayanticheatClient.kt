7520d24e3fb36ccb430352d8091c954308fc98ee
NoahOnFyre <noahonfyre@gmail.com>
2026-06-24 19:51:52 +0200

    Initial commit

---
+++ src/client/kotlin/com/nyronium/client/RelayanticheatClient.kt
@@ -0,0 +1,9 @@
+package com.nyronium.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object RelayanticheatClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
