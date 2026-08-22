f2c3f2d387d15d69374f72262896d9541a8c97d6
NoahOnFyre <noahonfyre@gmail.com>
2026-08-22 18:20:00 +0200

    feat: change default mod flagging mod to WHITELIST

--- src/main/kotlin/com/nyronium/config/TrustedConfigData.kt
+++ src/main/kotlin/com/nyronium/config/TrustedConfigData.kt
@@ -2,7 +2,7 @@ package com.nyronium.config
 
 class TrustedConfigData {
     var modList = mutableListOf<String>()
-    var mode = ConfigMode.BLACKLIST
+    var mode = ConfigMode.WHITELIST
     var bypass = mutableListOf<String>()
 
     enum class ConfigMode {
